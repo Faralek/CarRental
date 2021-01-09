4a30f7bff31a9d90c128c352bcaac1ebe127a74e
48727 <swierczynski093@gmail.com>
2021-01-09 22:15:41 +0100

    add online information sources

---
+++ src/main/java/com/kodilla/rentalcars/webServices/GasPrice.java
@@ -0,0 +1,24 @@
+package com.kodilla.rentalcars.webServices;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+
+public class GasPrice {
+
+    public String getGasPrice() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://gas-price.p.rapidapi.com/europeanCountries")
+                .get()
+                .addHeader("x-rapidapi-key", "42995f5c38msh34d44863f18a2ffp1d27b8jsn3bae39b0ab2d")
+                .addHeader("x-rapidapi-host", "gas-price.p.rapidapi.com")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return response.body().string();
+    }
+}
---
+++ src/main/java/com/kodilla/rentalcars/webServices/WeatherAlert.java
@@ -0,0 +1,24 @@
+package com.kodilla.rentalcars.webServices;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.io.IOException;
+import java.util.Objects;
+
+public class WeatherAlert {
+
+    public String getWeatherAlert() throws IOException {
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url("https://aerisweather1.p.rapidapi.com/alerts/jordan,mt")
+                .get()
+                .addHeader("x-rapidapi-key", "c2f4f4d115mshe35677673111b30p16edcejsn0e910caeaef4")
+                .addHeader("x-rapidapi-host", "aerisweather1.p.rapidapi.com")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return Objects.requireNonNull(response.body()).string();
+    }
+}
